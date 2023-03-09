530b8f5b8271928137e359698654129c88590dec
itlleat <scott.schulman84@gmail.com>
2023-03-09 10:42:57 -0700

    still in cors hell

--- Main/server.js
+++ Main/server.js
@@ -39,9 +39,12 @@
 // // // need to fill in the code for steps 10 and 11 to read and write data to the db.json file.
 
 const express = require('express');
-// const cors = require('cors');
+const cors = require('cors');
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
+const path = require("path");
+const fs = require("fs");
+const dbData = require("./db/db.json");
 
 // Initialize the app and create a port
 const app = express();
@@ -53,13 +56,93 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
   });
+
+
+// app.get("/", (req, res) => {
+//     res.sendFile(path.join(__dirname, "public/index.html"));
+//   });
   
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-// app.use(cors());
-app.use(express.static('public'));
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+//   //todo - * directory thing
+  
+//   app.get("/notes", (req, res) => {
+//     res.sendFile(path.join(__dirname, "public/notes.html"));
+//   });
+  
+//   app.get("/api/notes", (req, res) => res.json(dbData));
+  
+//   app.post("/api/notes", (req, res) => {
+//     const newNote = req.body;
+//     newNote.id = uuid.v4();
+//     const currentNotes = JSON.parse(fs.readFileSync("./db/db.json"));
+//     // Add the new note to the array of notes
+//     currentNotes.push(newNote);
+//     // Write the updated array of notes to db.json
+//     fs.writeFileSync("./db/db.json", JSON.stringify(currentNotes));
+//     res.json(newNote);
+//     console.info(`${req.method} request received`);
+//   });
+  
+//   app.delete("/api/notes/:id", (req, res) => {
+//     const noteId = req.params.id;
+//     // Read the current notes from db.json
+//     const currentNotes = JSON.parse(fs.readFileSync("./db/db.json"));
+//     // Filter out the note with the specified ID
+//     const updatedNotes = currentNotes.filter((note) => note.id !== noteId);
+//     // Write the updated array of notes to db.json
+//     fs.writeFileSync("./db/db.json", JSON.stringify(updatedNotes));
+//     res.json({ message: 'Note deleted successfully' });
+//     console.info('Note deleted successfully');
+//   });
+  
+// //   app.listen(PORT, () => {
+// //     console.log(`Server listening at http://localhost:${PORT}`);
+// //   });
+  
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// // app.use(cors());
+// app.use(express.static('public'));
+// app.use('/api', apiRoutes);
+// app.use('/', htmlRoutes);
+
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+  });
+  
+  app.get('/send', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/sendFile.html'))
+);
+  
+  app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+  });
+  
+  app.get("/api/notes", (req, res) => res.json(dbData));
+  
+  app.post("/api/notes", (req, res) => {
+    const newNote = req.body;
+    newNote.id = uuid.v4();
+    const currentNotes = JSON.parse(fs.readFileSync("./db/db.json"));
+    // Add the new note to the array of notes
+    currentNotes.push(newNote);
+    // Write the updated array of notes to db.json
+    fs.writeFileSync("./db/db.json", JSON.stringify(currentNotes));
+    res.json(newNote);
+    console.info(`${req.method} request received`);
+  });
+  
+  app.delete("/api/notes/:id", (req, res) => {
+    const noteId = req.params.id;
+    // Read the current notes from db.json
+    const currentNotes = JSON.parse(fs.readFileSync("./db/db.json"));
+    // Filter out the note with the specified ID
+    const updatedNotes = currentNotes.filter((note) => note.id !== noteId);
+    // Write the updated array of notes to db.json
+    fs.writeFileSync("./db/db.json", JSON.stringify(updatedNotes));
+    res.json({ message: 'Note deleted successfully' });
+    console.info('Note deleted successfully');
+  });
 
 // Start the server on the port
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
