cca3d4f47bc4d88f4d1f9575408db85948d7fdaf
itlleat <scott.schulman84@gmail.com>
2023-03-10 17:51:21 -0700

    added class solved examples from the week in test.js

---
+++ Develop/test.js
@@ -0,0 +1,525 @@
+//Activity one
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3001;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.send('Navigate to /send or /routes'));
+
+app.get('/send', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/send.html'))
+);
+
+app.get('/paths', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/paths.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
+
+//Activity 2
+
+// Require the JSON file and assign it to a variable called `termData`
+const termData = require('./terms.json');
+const PORT = 3001;
+
+// Initialize our app variable by setting it to the value of express()
+const app = express();test
+
+app.get('/', (req, res) => res.send('Visit http://localhost:3001/api'));
+
+// res.json() allows us to return JSON instead of a buffer, string, or static file
+app.get('/api', (req, res) => res.json(termData));
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
+
+//Activity 3
+
+const express = require('express');
+const { sortData } = require('./sortData');
+const termData = require('./terms.json');
+
+const PORT = 3001;
+
+const app = express();
+
+const sortHelper = (type) =>
+  termData.sort(sortData('term', 'relevance', `${type}`));
+
+// GET route to get all of the terms or all the terms sorted
+app.get('/api/terms/', (req, res) => {
+  // console.log(req.params, 'first');
+
+  // Check and see if there is a query parameter at all
+  const hasQuery = Object.keys(req.query).length > 0;
+
+  // If we have a query of 'sort' and it's value is 'dsc' send the results in descending order
+  if (hasQuery && req.query.sort === 'dsc') {
+    return res.json(sortHelper('dsc'));
+  }
+
+  // If we have a query of 'sort' and it's value is 'asc' send the results in ascending order
+  if (hasQuery && req.query.sort === 'asc') {
+    return res.json(sortHelper('asc'));
+  }
+
+  // If there is no query parameter, return terms
+  return res.json(termData);
+});
+
+// GET route that returns any specific term
+app.get('/api/term/:term', (req, res) => {
+  const requestedTerm = req.params.term.toLowerCase();
+
+  // Iterate through the terms name to check if it matches `req.params.term`
+  if (requestedTerm) {
+    for (let i = 0; i < termData.length; i++) {
+      if (requestedTerm === termData[i].term.toLowerCase()) {
+        return res.json(termData[i]);
+      }
+    }
+  }
+
+  // Return a message if the term doesn't exist in our DB
+  return res.json('No term found');
+});
+
+// GET route for returning all terms from a given category
+app.get('/api/terms/:category', (req, res) => {
+  const requestedCategory = req.params.category.toLowerCase();
+  const result = [];
+
+  for (let i = 0; i < termData.length; i++) {
+    const currentTermCategory = termData[i].category;
+    if (requestedCategory === currentTermCategory) {
+      result.push(termData[i]);
+    }
+  }
+  return res.json(result);
+});
+
+app.get('/api/categories', (req, res) => {
+  // Make an array with all the categories
+  const categories = termData.map((term) => term.category);
+
+  // Filter duplicate categories from the array
+  const result = categories.filter((cat, i) => categories.indexOf(cat) === i);
+
+  // Return the result
+  return res.json(result);
+});
+
+// Listen for connections
+app.listen(PORT, () =>
+  console.info(`Example app listening at http://localhost:${PORT} 🚀`)
+);
+
+//Activity 4
+const express = require('express');
+const path = require('path');
+const petData = require('./db/animals.json');
+
+const PORT = 3001;
+
+const app = express();
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/api/pets', (req, res) => res.json(petData));
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
+
+//Activity 5
+
+const express = require('express');
+
+const app = express();
+const PORT = process.env.port || 3001;
+const path = require('path');
+
+// Serve images, css files, js files from the public directory
+// Allows us to reference files with their relative path
+// Example: http://localhost:3001/images/gh.png
+
+// Add a static middleware for serving assets in the public folder
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/')));
+
+app.listen(PORT, () =>
+  console.log(`Serving static asset routes at http://localhost:${PORT}!`)
+);
+
+//Activity 6
+
+const express = require('express');
+const pulls = require('./db/repos.json');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send(
+    'Use the API endpoint at <a href="http://localhost:3001/api">localhost:3001/api</a>'
+  );
+});
+
+// Endpoint to return our own locally stored data
+app.get('/api', (req, res) => res.json(pulls));
+
+app.listen(3001, () => console.log('Express Server on port 3001!'));
+
+//Activity 7
+
+const express = require('express');
+
+const PORT = 3001;
+
+const app = express();
+
+// GET request for reviews
+app.get('/api/reviews', (req, res) => {
+  // Inform the client
+  res.json(`${req.method} request received to get reviews`);
+
+  // Log our request to the terminal
+  console.info(`${req.method} request received to get reviews`);
+});
+
+// POST request for reviews
+app.post('/api/reviews', (req, res) => {
+  // Inform the client that their POST request was received
+  res.json(`${req.method} request received to add a review`);
+
+  // Log our request to the terminal
+  console.info(`${req.method} request received to add a review`);
+});
+
+// GET request for upvotes
+app.get('/api/upvotes', (req, res) => {
+  // Inform the client
+  res.json(`${req.method} request received to retrieve upvote count`);
+
+  // Log our request to the terminal
+  console.info(`${req.method} request received to retrieve upvote count`);
+});
+
+// POST request
+app.post('/api/upvotes', (req, res) => {
+  // Inform the client that their POST request was received
+  res.json(`${req.method} request received to upvote`);
+
+  // Log our request to the terminal
+  console.info(`${req.method} request received to upvote`);
+});
+
+app.listen(PORT, () =>
+  console.log(`Express server listening on port ${PORT}!`)
+);
+
+//Activity 8
+
+const express = require('express');
+
+const PORT = 3001;
+const reviews = require('./db/reviews');
+
+const app = express();
+
+// Middleware for parsing application/json
+app.use(express.json());
+
+// Middleware for urlecoded data
+// `urlencoded` data represents a URL encoded form. If we had a HTML form with fields: `username` and `password`, our urlencoded data would be "username=JohnAppleseed&password=passw0rd"
+// This middleware will parse that string into an object containing key value pairs
+app.use(express.urlencoded({ extended: true }));
+
+// GET request for ALL reviews
+app.get('/api/reviews', (req, res) => {
+  // Log our request to the terminal
+  console.info(`${req.method} request received to get reviews`);
+
+  // Sending all reviews to the client
+  return res.status(200).json(reviews);
+});
+
+// GET request for a single review
+app.get('/api/reviews/:review_id', (req, res) => {
+  if (req.params.review_id) {
+    console.info(`${req.method} request received to get a single a review`);
+    const reviewId = req.params.review_id;
+    for (let i = 0; i < reviews.length; i++) {
+      const currentReview = reviews[i];
+      if (currentReview.review_id === reviewId) {
+        res.status(200).json(currentReview);
+        return;
+      }
+    }
+    res.status(404).send('Review not found');
+  } else {
+    res.status(400).send('Review ID not provided');
+  }
+});
+
+// POST request to add a review
+app.post('/api/reviews', (req, res) => {
+  // Log that a POST request was received
+  console.info(`${req.method} request received to add a review`);
+
+  // Prepare a response object to send back to the client
+  let response;
+
+  // Check if there is anything in the response body
+  if (req.body && req.body.product) {
+    response = {
+      status: 'success',
+      data: req.body,
+    };
+    res.status(201).json(response);
+  } else {
+    res.status(400).json('Request body must at least contain a product name');
+  }
+
+  // Log the response body to the console
+  console.log(req.body);
+});
+
+// POST request to upvote a review
+app.post('/api/upvotes/:review_id', (req, res) => {
+  if (req.body && req.params.review_id) {
+    console.info(`${req.method} request received to upvote a review`);
+    const reviewId = req.params.review_id;
+    for (let i = 0; i < reviews.length; i++) {
+      const currentReview = reviews[i];
+      if (currentReview.review_id === reviewId) {
+        currentReview.upvotes += 1;
+        res.status(200).json(`New upvote count is: ${currentReview.upvotes}!`);
+        return;
+      }
+    }
+    res.status(404).json('Review ID not found');
+  }
+});
+
+app.listen(PORT, () =>
+  console.log(`Express server listening on port ${PORT}!`)
+);
+
+//Activity 9
+
+const express = require('express');
+const path = require('path');
+// Helper method for generating unique ids
+const uuid = require('./helpers/uuid');
+const reviews = require('./db/reviews');
+
+const PORT = 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET request for reviews
+app.get('/api/reviews', (req, res) => {
+  console.info(`GET /api/reviews`);
+  res.status(200).json(reviews);
+});
+
+// GET a single review
+app.get('/api/reviews/:review_id', (req, res) => {
+  if (req.params.review_id) {
+    console.info(`${req.method} request received to get a single a review`);
+    const reviewId = req.params.review_id;
+    for (let i = 0; i < reviews.length; i++) {
+      const currentReview = reviews[i];
+      if (currentReview.review_id === reviewId) {
+        res.json(currentReview);
+        return;
+      }
+    }
+    res.status(404).send('Review not found');
+  } else {
+    res.status(400).send('Review ID not provided');
+  }
+});
+
+// POST request to add a review
+app.post('/api/reviews', (req, res) => {
+  // Log that a POST request was received
+  console.info(`${req.method} request received to add a review`);
+
+  // Destructuring assignment for the items in req.body
+  const { product, review, username } = req.body;
+
+  // If all the required properties are present
+  if (product && review && username) {
+    // Variable for the object we will save
+    const newReview = {
+      product,
+      review,
+      username,
+      upvotes: Math.floor(Math.random() * 100),
+      review_id: uuid(),
+    };
+
+    const response = {
+      status: 'success',
+      body: newReview,
+    };
+
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json('Error in posting review');
+  }
+});
+
+// GET request for a specific review's upvotes
+app.get('/api/upvotes/:review_id', (req, res) => {
+  console.info(`${req.method} request received to get upvotes for a review`);
+  for (let i = 0; i < reviews.length; i++) {
+    const currentReview = reviews[i];
+    if (currentReview.review_id === req.params.review_id) {
+      res.status(200).json({
+        message: `The review with ID ${currentReview.review_id} has ${currentReview.upvotes}`,
+        upvotes: currentReview.upvotes,
+      });
+      return;
+    }
+  }
+  res.status(404).json('Review ID not found');
+});
+
+// POST request to upvote a review
+app.post('/api/upvotes/:review_id', (req, res) => {
+  if (req.body && req.params.review_id) {
+    console.info(`${req.method} request received to upvote a review`);
+    const reviewId = req.params.review_id;
+    for (let i = 0; i < reviews.length; i++) {
+      const currentReview = reviews[i];
+      if (currentReview.review_id === reviewId) {
+        currentReview.upvotes += 1;
+        res.status(200).json(`New upvote count is: ${currentReview.upvotes}!`);
+        return;
+      }
+    }
+    res.status(404).json('Review ID not found');
+  }
+});
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
+
+//Activity 10
+
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+// Helper method for generating unique ids
+const uuid = require('./helpers/uuid');
+
+const PORT = 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET request for reviews
+app.get('/api/reviews', (req, res) => {
+  // Send a message to the client
+  res.status(200).json(`${req.method} request received to get reviews`);
+
+  // Log our request to the terminal
+  console.info(`${req.method} request received to get reviews`);
+});
+
+// POST request to add a review
+app.post('/api/reviews', (req, res) => {
+  // Log that a POST request was received
+  console.info(`${req.method} request received to add a review`);
+
+  // Destructuring assignment for the items in req.body
+  const { product, review, username } = req.body;
+
+  // If all the required properties are present
+  if (product && review && username) {
+    // Variable for the object we will save
+    const newReview = {
+      product,
+      review,
+      username,
+      review_id: uuid(),
+    };
+
+    // Obtain existing reviews
+    fs.readFile('./db/reviews.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        // Convert string into JSON object
+        const parsedReviews = JSON.parse(data);
+
+        // Add a new review
+        parsedReviews.push(newReview);
+
+        // Write updated reviews back to the file
+        fs.writeFile(
+          './db/reviews.json',
+          JSON.stringify(parsedReviews, null, 4),
+          (writeErr) =>
+            writeErr
+              ? console.error(writeErr)
+              : console.info('Successfully updated reviews!')
+        );
+      }
+    });
+
+    const response = {
+      status: 'success',
+      body: newReview,
+    };
+
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json('Error in posting review');
+  }
+});
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
+
+//Activity 11
+
