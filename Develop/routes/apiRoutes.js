097b1277eaf61509df4c75afff0024d7ac64e835
itlleat <scott.schulman84@gmail.com>
2023-03-11 21:51:41 -0700

    broke through one bug into a POST error index.js late 30s

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,28 +1,31 @@
-const router = require('express').Router();
-const store = require('../db/store');
+const router = require("express").Router();
+const store = require("../db/store");
+// const PORT = const PORT = process.env.PORT || 3001;
 
 router.get('/notes', (req, res) => {
-    store   
-        .getNotes()
-        .then((notes) => {
-            return res.json(notes);
-        })
-        .catch((err) => res.status(500).json(err));
+  store
+    .getNotes()
+    .then((notes) => {
+      return res.json(notes);
+    })
+    .catch((err) => res.status(500).json(err));
+  return;
 });
 
-router.post('/notes', (req, res) => {
-    store
-        .addNote(req.body)
-        .then((note) => res.json(note))
-        .cath((err) => res.json(err));
+router.post("/notes", (req, res) => {
+  store
+    .addNote(req.body)
+    .then((note) => res.json(note))
+    .catch((err) => res.json(err));
+  return;
 });
 
-router.delete('/notes/:id', (req, res) => {
-    store
-      .removeNote(req.params.id)
-      .then(() => res.json({ ok: true }))
-      .catch((err) => res.status(500).json(err));
-  });
-  
+router.delete("/notes/:id", (req, res) => {
+  store
+    .removeNote(req.params.id)
+    .then(() => res.json({ ok: true }))
+    .catch((err) => res.status(500).json(err));
+  return;
+});
 
 module.exports = router;
--- Develop/server.js
+++ Develop/server.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 const express = require('express');
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
