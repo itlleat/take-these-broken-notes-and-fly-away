8309cfa0ee70ca322635ae44ac779a637a91e7a8
itlleat <scott.schulman84@gmail.com>
2023-03-11 20:04:40 -0700

    almost there running into a index.js error the browser thinks it's the index.html document

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,7 +1,7 @@
-const router = require('express').router();
-const store = require('../store');
+const router = require('express').Router();
+const store = require('../db/store');
 
-router.get("/notes", (req, res) => {
+router.get('/notes', (req, res) => {
     store   
         .getNotes()
         .then((notes) => {
@@ -10,20 +10,19 @@ router.get("/notes", (req, res) => {
         .catch((err) => res.status(500).json(err));
 });
 
-router.post("/notes", (req, res) => {
+router.post('/notes', (req, res) => {
     store
         .addNote(req.body)
         .then((note) => res.json(note))
         .cath((err) => res.json(err));
 });
 
-app.delete("/api/notes/:id", (req, res) => {
-    const noteId = req.params.id;
-    const currentNotes = JSON.parse(fs.readFileSync("./db/db.json"));
-    const updatedNotes = currentNotes.filter((note) => note.id !== noteId);
-    fs.writeFileSync("./db/db.json", JSON.stringify(updatedNotes));
-    res.json({ message: 'Note deleted successfully' });
-    console.info('Note deleted successfully');
-});
+router.delete('/notes/:id', (req, res) => {
+    store
+      .removeNote(req.params.id)
+      .then(() => res.json({ ok: true }))
+      .catch((err) => res.status(500).json(err));
+  });
+  
 
 module.exports = router;
